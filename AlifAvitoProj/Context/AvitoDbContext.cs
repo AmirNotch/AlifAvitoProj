58fcd34e9903ea66a4cd1cd5d598cb0d956040af
Amir Ibragimov <ibragimov-amir@list.ru>
2021-11-27 10:15:30 +0600

    Added AvitoDbContext

---
+++ AlifAvitoProj/Context/AvitoDbContext.cs
@@ -0,0 +1,51 @@
+﻿using AlifAvitoProj.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlifAvitoProj.Context
+{
+    public class AvitoDbContext : DbContext
+    {
+        public AvitoDbContext(DbContextOptions options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public virtual DbSet<Advert> Adverts { get; set; } 
+        public virtual DbSet<User> Users { get; set; } 
+        public virtual DbSet<Category> Categories { get; set; } 
+        public virtual DbSet<City> Cities { get; set; } 
+        public virtual DbSet<Review> Reviews { get; set; } 
+        public virtual DbSet<Reviewer> Reviewers { get; set; } 
+        public virtual DbSet<AdvertUser> AdvertUsers { get; set; } 
+        public virtual DbSet<AdvertCategory> AdvertCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AdvertCategory>()
+                        .HasKey(bc => new { bc.AdvertId, bc.CategoryId });
+            modelBuilder.Entity<AdvertCategory>()
+                        .HasOne(b => b.Advert)
+                        .WithMany(bc => bc.AdvertCategories)
+                        .HasForeignKey(b => b.AdvertId);
+            modelBuilder.Entity<AdvertCategory>()
+                        .HasOne(c => c.Category)
+                        .WithMany(bc => bc.AdvertCategories)
+                        .HasForeignKey(c => c.CategoryId);
+
+            modelBuilder.Entity<AdvertUser>()
+                        .HasKey(ba => new { ba.AdvertId, ba.UserId });
+            modelBuilder.Entity<AdvertUser>()
+                        .HasOne(b => b.Advert)
+                        .WithMany(ba => ba.AdvertUsers)
+                        .HasForeignKey(b => b.AdvertId);
+            modelBuilder.Entity<AdvertUser>()
+                        .HasOne(a => a.User)
+                        .WithMany(ba => ba.AdvertUsers)
+                        .HasForeignKey(a => a.UserId);
+        }
+    }
+}
